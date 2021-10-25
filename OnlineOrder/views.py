61a562a388e71750c32d3bc47d794d7103e7d7e1
Hantsi <hantsitaumang@gmail.com>
2021-10-25 09:18:11 +0200

    img/con/ord

--- OnlineOrder/views.py
+++ OnlineOrder/views.py
@@ -15,25 +15,27 @@ def contacts(request):
         message_form = messageform(request.POST)
         if message_form.is_valid():
             message_form.save();
-            messages.info(request, "Thank you....we will stay in contact")
+            message = True
             return redirect('contacts')
         else:
             messages.info(request, "Enter valid information")
             return redirect('contacts')
     else:
         message_form = messageform()
-    return render(request, "contacts.html", {'message_form':message_form})
+        message = False
+    return render(request, "contacts.html", {'message_form':message_form, 'message':message})
 
 def order(request):
     if request.method == 'POST':
         order_form = orderform(request.POST)
         if order_form.is_valid():
             order_form.save();
-            messages.info(request, "Your Order has been received....we will stay in contact")
+            message = True
             return redirect('order')
         else:
             messages.info(request, "Enter valid information")
             return redirect('order')
     else:
         order_form = orderform()
-    return render(request, "order.html", {'order_form':order_form})
+        message = False
+    return render(request, "order.html", {'order_form':order_form, 'message':message})
--- SHEorganics/settings.py
+++ SHEorganics/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_)q+!^9y#p50*w&m9$-6!tfrh7vdx$h(hhe=%i6=m(+9_1e868'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG", False))
